41e91c58dc747d055c3cc5c805ca65bd4e892bc1
elis8403 <elis8403@kea.stud.dk>
2021-01-12 12:53:10 +0100

    valideret

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function onClickMenu() {
     document.getElementById("menu").classList.toggle("change");
-
     document.getElementById("nav").classList.toggle("change");
     console.log("change");
 }
